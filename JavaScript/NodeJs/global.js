d443077afa306708ee1dcf58df9598ce804deb14
Segun <laiyemosegun@gmail.com>
2022-02-03 18:20:11 +0100

    Done

---
+++ JavaScript/NodeJs/global.js
@@ -0,0 +1,2 @@
+console.log("Hello from Mpape");
+console.log(__filename);
---
+++ Node/global.js
@@ -0,0 +1,5 @@
+var seg = "Shegun Laiyemo";
+
+var newSeg = seg.toUpperCase(seg);
+
+global.console.log('newSeg ${newSeg}');
